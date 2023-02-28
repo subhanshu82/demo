81ab0dccfe770fc6fbd1c2fc4ffd5581a1cc0794
subhanshu <subhanshu@mindpath>
2023-02-28 14:43:28 +0530

    test

--- test.py
+++ test.py
@@ -3,10 +3,6 @@ class demo:
     def __init__(self) -> None:
          pass
 
-    def demo(self):
-        print("demo")
-        return
-    
-    def train(self):
-        print("train")
+    def test(self):
+        print("demo1")
         return
